2a951436d0d7764a499bea2012fe94cf8b49a290
Andre Jones <andrejones92@gmail.com>
2021-11-22 01:19:22 -0500

    clsoe all modals with "x" only

--- src/app/components/ready-for-publication-modal/ready-for-publication-modal.component.ts
+++ src/app/components/ready-for-publication-modal/ready-for-publication-modal.component.ts
@@ -22,8 +22,8 @@ export class ReadyForPublicationModalComponent implements OnInit {
 
   openreadyForPublicationModal(readyForPublicationDialog: NgbModal) {
     this.modalService.open(readyForPublicationDialog, {
-      backdrop : 'static',
-      keyboard : false,
+      backdrop: 'static',
+      keyboard: false,
       windowClass: 'ready-for-publication-modal'
     });
   }
