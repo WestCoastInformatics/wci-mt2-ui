47bb601748bded872dfb6864079738fd2b5ae7cc
Andre Jones <andrejones92@gmail.com>
2021-12-29 16:02:15 -0500

    bug fix for published refsets

--- src/app/pages/refset-details.ts
+++ src/app/pages/refset-details.ts
@@ -389,7 +389,7 @@ export class RefsetDetails {
                     tags: this.refsetData.tags,
                     referenceType: this.refsetData.type,
                     privateRefset: this.refsetData.privateRefset,
-                    versionDate: this.refsetData.editOriginBranchPath.split('/')[1],
+                    versionDate: this.refsetData.versionDate ? this.refsetData.versionDate : this.refsetData.editOriginBranchPath.split('/')[1],
                     versionNotes: this.refsetData.versionNotes
                 };
 
