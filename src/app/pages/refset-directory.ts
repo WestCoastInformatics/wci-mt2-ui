f462fe27f66320693f4930d035d3d2ab69f7a367
Andre Jones <andrejones92@gmail.com>
2021-12-30 19:55:55 -0500

    replace forward slash with empty space for filter

--- src/app/pages/refset-directory.ts
+++ src/app/pages/refset-directory.ts
@@ -249,7 +249,7 @@ export class RefsetDirectory implements OnInit, AfterViewInit {
                 };
 
                 if (CodeUtility.hasValue(query)) {
-                    query = query.replace('/', '%2F');
+                    query = query.replace('/', ' ');
                     restParams.query = query;
                 }
 
