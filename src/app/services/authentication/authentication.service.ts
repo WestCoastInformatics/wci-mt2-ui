e6295aeb75110cba3ac381a06923f04da461c61f
Andre Jones <andrejones92@gmail.com>
2022-02-15 10:56:42 -0500

    log in to dashboard instead of directory

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -66,7 +66,7 @@ export class AuthenticationService {
 
                 localStorage.setItem('auth_token', data.authToken);
                 localStorage.setItem('refset_user', JSON.stringify(data));
-                this.router.navigate(['directory']);
+                this.router.navigate(['projects']);
                 this.userSubject.next(userData);
                 this.isUserLoggedIn = true;
             },
