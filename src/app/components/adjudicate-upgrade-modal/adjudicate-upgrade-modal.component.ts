68a93a3f64a240cdf46f6831e90532057d65d4a1
Andre Jones <andrejones92@gmail.com>
2022-03-08 10:47:40 -0500

    selected language

--- src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
+++ src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
@@ -77,7 +77,7 @@ export class AdjudicateUpgradeModalComponent implements OnInit, AfterViewInit, O
     this.languageOptions = this.refsetData?.edition?.fullyQualifiedLanguageRefsets.map((x) => {
       return x.qualifiedLanguageCode;
     });
-    this.selectedLanguage = this.languageOptions[0];
+    // this.selectedLanguage = this.languageOptions[0];
   }
 
   ngAfterViewInit() {
