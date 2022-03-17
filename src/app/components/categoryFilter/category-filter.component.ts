5bcadbbee48813b3feb8df40ae91cee03c0416bb
twkatadin <twkatadin@gmail.com>
2022-03-16 14:53:56 -1000

    RT2-731: Added filter dropdowns for date and status on projects page.

--- src/app/components/categoryFilter/category-filter.component.ts
+++ src/app/components/categoryFilter/category-filter.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { AgFrameworkComponent } from 'ag-grid-angular';
 import { IFloatingFilter, IFloatingFilterParams, TextFilter, TextFilterModel } from 'ag-grid-community';
 
 export interface SelectFloatingFilterParams extends IFloatingFilterParams {
+
 	selectedValue: string;
 	names: Array<any>;
 }
@@ -12,63 +13,63 @@ export interface SelectFloatingFilterParams extends IFloatingFilterParams {
 	templateUrl: 'category-filter.component.html'
 })
 export class CategoryFilterComponent implements IFloatingFilter, AgFrameworkComponent<SelectFloatingFilterParams> {
+
 	params: SelectFloatingFilterParams;
 	currentValue;
-
 	optionNum: number = 0;
 	names: Array<any>;
 	options: Array<SelectEntry> = [];
-
 	selectedOption = this.options[0];
 
-
-
 	agInit(params: SelectFloatingFilterParams): void {
+
 		this.params = params;
 		this.names = this.params.names;
-
 		let obj: SelectEntry = new SelectEntry(this.optionNum++, "");
 		this.options.push(obj);
+
 		for (let i = 0; i < this.names?.length; i++) {
+
 			let entry = this.names[i];
+
 			if (entry.type === "status") {
-				let obj: SelectEntry = new SelectEntry(this.optionNum++, entry.value);
+
+				let obj: SelectEntry = new SelectEntry(this.optionNum++, entry.name, entry.value);
 				this.options.push(obj);
 			} else {
+
 				let obj: SelectEntry = new SelectEntry(this.optionNum++, entry.value.charAt(0) + entry.value.slice(1).toLowerCase());
 				this.options.push(obj);
 			}
 		}
-
 	}
 
 	valueChanged() {
 
-		let valueToUse = this.selectedOption.name != null ? this.selectedOption.name : "";
-		//let valueToUse = this.currentValue === '' ? null : this.selectedOption.name;
-		this.params.parentFilterInstance((instance: TextFilter) =>
-			instance.onFloatingFilterChanged('equals', valueToUse === '' ? null : valueToUse));
+		let valueToUse = this.selectedOption.value != null ? this.selectedOption.value : "";
+		this.params.parentFilterInstance((instance: TextFilter) => instance.onFloatingFilterChanged('equals', valueToUse === '' ? null : valueToUse));
 	}
 
 	onParentModelChanged(parentModel: TextFilterModel): void {
 
 		if (!parentModel) {
-			this.selectedOption.name = "";
-		}
-		else {
-			this.selectedOption.name = parentModel.filter;
+			this.selectedOption.value = "";
+		} else {
+			this.selectedOption.value = parentModel.filter;
 		}
 	}
 }
 
 class SelectEntry {
 
-	public num: any;
-	public name: string;
+	public index: any;
+	public label: string;
+	public value: string;
 
-	constructor(num: any, name: string) {
-		this.num = num;
-		this.name = name;
+	constructor(index: any, value: string, label: string = value) {
+
+		this.index = index;
+		this.label = label;
+		this.value = value;
 	}
-
 }
