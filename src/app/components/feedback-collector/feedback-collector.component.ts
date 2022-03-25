9c3900f28c5e52d06acb5049328735f134c355c8
njmarques <nuno.marques@gmail.com>
2022-03-25 11:28:24 -0700

    Add JIRA feedback collector to application

---
+++ src/app/components/feedback-collector/feedback-collector.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'feedback-collector',
+    templateUrl: './feedback-collector.component.html'
+})
+export class FeedbackCollectorComponent {
+
+    constructor() {
+    }
+
+}
