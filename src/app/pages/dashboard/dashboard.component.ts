a952d2c8bb1dab5da20145acc1474bf7b80064f9
doublemwebsolutions <dev@doublemwebsolutions.com>
2022-03-27 20:19:10 -0600

    Dashboard Landing page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,6 +7,26 @@ import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
+  searchText = '';
+  
+  columnDefs = [
+    { field: 'refsetId', headerName: 'Reference Set', flex: 1, minWidth: 550, unSortIcon: true, sortable: true},
+    { field: 'workflowStatus', headerName: 'Current Workflow Status', unSortIcon: true, sortable: true},
+    { field: 'modified', tooltipField: 'modified', headerName: 'Last Modified', unSortIcon: true, sortable: true}
+  ];
+
+  data = [
+    {refsetId: 'IHTSDO/SNOMED International Project/Atherosclerotic cardiovascular disease (ASCVD) …', workflowStatus: 'In Review (MHarry)', modified: '2022-01-01'},
+    {refsetId: 'WestCoastInformatics/WCI Test MANAGED-SERVICE Project/Microorganism subset', workflowStatus: 'Ready for Review', modified: '2021-12-31'},
+    {refsetId: 'WestCoastInformatics/WCI Test MANAGED-SERVICE Project/Nursing Procedures', workflowStatus: 'Ready for Edit', modified: '2021-12-09'},
+    {refsetId: 'WestCoastInformatics/WCI Test MANAGED-SERVICE Project/Urval Covid-19', workflowStatus: 'In Edit (SWhalen)', modified: '2021-12-01'},
+    {refsetId: 'IHTSDO/SNOMED International Project/General Practice / Family Practice reference set', workflowStatus: 'Review Completed', modified: '2021-11-28'},
+    {refsetId: 'Organizaztion/Project/Place Holder reference set', workflowStatus: 'Review Completed', modified: '2021-10-28'},
+    {refsetId: 'Organizaztion/Project/Place Holder reference set', workflowStatus: 'Review Completed', modified: '2021-10-28'},
+    {refsetId: 'Organizaztion/Project/Place Holder reference set', workflowStatus: 'Review Completed', modified: '2021-10-28'},
+    {refsetId: 'Organizaztion/Project/Place Holder reference set', workflowStatus: 'Review Completed', modified: '2021-10-28'},
+    {refsetId: 'Organizaztion/Project/Place Holder reference set', workflowStatus: 'Review Completed', modified: '2021-10-28'}
+  ];
 
   constructor(private readonly breadcrumbService: BreadcrumbService, private readonly titleService: Title) { }
 
