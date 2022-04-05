014dc8e3cf4bf7076f391c9a21ced07356ab52cf
twkatadin <twkatadin@gmail.com>
2022-04-04 15:05:29 -1000

    RT2-805: added change report

--- src/app/utilities/ui.utility.ts
+++ src/app/utilities/ui.utility.ts
@@ -308,7 +308,9 @@ export class UiUtility {
                             conceptStatusArray.push({
                                 code: conceptId, 
                                 added: conceptStatus.operation == 'Added', 
-                                failed: conceptStatus.status == 'Failed', 
+                                failed: conceptStatus.status == 'Failed' || conceptStatus.status == 'Already Member', 
+                                operation: conceptStatus.operation,
+                                status: conceptStatus.status,
                                 name: conceptStatus.name, 
                                 active: conceptStatus.active
                             });
