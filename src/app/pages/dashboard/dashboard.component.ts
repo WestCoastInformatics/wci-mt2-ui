e2553a1c3e0119f627a25ff893a5550ac3b42c42
peter-va <pvanaus@ucdavis.edu>
2022-04-05 17:33:33 -0700

    1/2/4 in 853

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,84 +6,86 @@ import { RefsetService } from 'src/app/services/rest/refset.service';
 import { UiUtility } from 'src/app/utilities/ui.utility';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
-  searchText = '';
-  organizationList = [];
-  projectList = [];
-  teamList = [];
-  currentUser: any;
+    searchText = '';
+    organizationList = [];
+    projectList = [];
+    teamList = [];
+    currentUser: any;
 
-  columnDefs = [
-    { field: 'refsetId', headerName: 'Reference Set', flex: 1, minWidth: 550, unSortIcon: true, sortable: true},
-    { field: 'workflowStatus', headerName: 'Workflow Status', unSortIcon: true, sortable: true},
-    { field: 'modified', tooltipField: 'modified', headerName: 'Last Modified', unSortIcon: true, sortable: true, valueGetter:
-    UiUtility.gridDateValueGetter,}
-  ];
-
-  data = [];
-  api: any;
-  columnApi: any;
-
-  constructor(private readonly breadcrumbService: BreadcrumbService,
-    private readonly titleService: Title,
-    private readonly refsetService: RefsetService,
-    private readonly authService: AuthenticationService) { }
-
-  ngOnInit(): void {
-    this.titleService.setTitle('Refset Tool - Dashboard');
-    this.breadcrumbService.setBreadcrumbs([
-      { path: '/dashboard', label: 'Dashboard' }
-    ]);
-    this.currentUser = this.authService.getUser();
-    this.getOrganizations();
-    this.getProjects();
-    this.getTeams();
-  }
-
-
-  getOrganizations(): void {
-    this.refsetService.getOrganizations().subscribe((results) => {
-      this.organizationList = results.items;
-      console.log(this.organizationList);
-    });
-  }
-
-  onGridReady = (params) => {
-    this.api = params.api;
-    this.columnApi = params.columnApi;
-    this.getRefSets();
-  }
-
-  getRefSets(): void {
-    this.refsetService.getRefsets('limit=10&offset=0&sort=name&sortAscending=true').subscribe((x) => {
-      console.log(x.items);
-      for (let refset of x.items) {
-        if (refset.assignedUser === this.currentUser.userName) {
-          this.data.push({ refsetId: `${refset?.organizationName}/${refset?.project[0]?.name}/${refset.name}`, workflowStatus: `${refset?.workflowStatus}`, modified: `${refset?.modified}` })
+    columnDefs = [
+        { field: 'refsetId', headerName: 'Reference Set', flex: 1, minWidth: 550, unSortIcon: true, sortable: true },
+        { field: 'workflowStatus', headerName: 'Workflow Status', unSortIcon: true, sortable: true },
+        {
+            field: 'modified', tooltipField: 'modified', headerName: 'Last Modified', unSortIcon: true, sortable: true, valueGetter:
+                UiUtility.gridDateValueGetter,
         }
-      }
-      console.log(this.data)
-      this.api.setRowData(this.data.slice(0, 10));
-      this.api.redrawRows();
-    });
-  }
+    ];
 
-  getProjects(): void {
-    this.refsetService.getProjects('limit=500&offset=0&sort=name&sortAscending=true').subscribe((results) => {
-      this.projectList = results.items;
-    });
-  }
+    data = [];
+    api: any;
+    columnApi: any;
 
-  getTeams(): void {
-    this.refsetService.getTeams('limit=500&offset=0&sort=name&sortAscending=true').subscribe((results) => {
-      this.teamList = results.items.filter((x) => {
-        return x.members.some((member) => {
-          return member.includes(this.currentUser.id);
+    constructor(private readonly breadcrumbService: BreadcrumbService,
+        private readonly titleService: Title,
+        private readonly refsetService: RefsetService,
+        private readonly authService: AuthenticationService) { }
+
+    ngOnInit(): void {
+        this.titleService.setTitle('Refset Tool - Dashboard');
+        this.breadcrumbService.setBreadcrumbs([
+            { path: '/dashboard', label: 'Dashboard' }
+        ]);
+        this.currentUser = this.authService.getUser();
+        this.getOrganizations();
+        this.getProjects();
+        this.getTeams();
+    }
+
+
+    getOrganizations(): void {
+        this.refsetService.getOrganizations().subscribe((results) => {
+            this.organizationList = results.items;
+            console.log(this.organizationList);
         });
-      });
-    });
-  }
+    }
+
+    onGridReady = (params) => {
+        this.api = params.api;
+        this.columnApi = params.columnApi;
+        this.getRefSets();
+    }
+
+    getRefSets(): void {
+        this.refsetService.getRefsets('limit=10&offset=0&sort=name&sortAscending=true').subscribe((x) => {
+            console.log(x.items);
+            for (let refset of x.items) {
+                if (refset.assignedUser === this.currentUser.userName) {
+                    this.data.push({ refsetId: `${refset?.organizationName}/${refset?.project?.name}/${refset.name}`, workflowStatus: `${refset?.workflowStatus}`, modified: `${refset?.modified}` })
+                }
+            }
+            console.log(this.data)
+            this.api.setRowData(this.data.slice(0, 10));
+            this.api.redrawRows();
+        });
+    }
+
+    getProjects(): void {
+        this.refsetService.getProjects('limit=500&offset=0&sort=name&sortAscending=true').subscribe((results) => {
+            this.projectList = results.items;
+        });
+    }
+
+    getTeams(): void {
+        this.refsetService.getTeams('limit=500&offset=0&sort=name&sortAscending=true').subscribe((results) => {
+            this.teamList = results.items.filter((x) => {
+                return x.members.some((member) => {
+                    return member.includes(this.currentUser.id);
+                });
+            });
+        });
+    }
 }
