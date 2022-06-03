429ddf41a614c862f36eee2ad15274816cf31fe1
doublemwebsolutions <dev@doublemwebsolutions.com>
2022-06-03 13:01:57 -0600

    Dashboard - Don't render screen until finished loading

--- src/app/pages/organizations/people/people.component.ts
+++ src/app/pages/organizations/people/people.component.ts
@@ -118,6 +118,7 @@ export class OrganizationPeopleComponent implements OnInit {
 
 		this.gridParams = params;
 		this.gridApi = params.api;
+		this.gridApi.setRowData([]);
 		this.getPeople();
 	}
 
