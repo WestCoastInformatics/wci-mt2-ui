8d6cc5c2af8cbc249f5ea2e372a461871241b663
doublemwebsolutions <dev@doublemwebsolutions.com>
2022-06-09 15:30:42 -0600

    Dashboard - Remove Email from Project-Create/Update Forms

--- src/app/components/create-new-project-modal/create-new-project-modal.component.ts
+++ src/app/components/create-new-project-modal/create-new-project-modal.component.ts
@@ -105,7 +105,7 @@ export class CreateNewProjectModalComponent {
             active: true,
             name: this.name,
             description: this.description,
-            primaryContactEmail: this.email,
+            //primaryContactEmail: this.email,
             privateProject: this.privateProject,
             teams: [],
             organization: this.selectedOrganization
