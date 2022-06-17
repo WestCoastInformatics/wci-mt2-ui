de9d94339c735d2a460065bc34cf6e3390964ca2
shapirod2 <dshapiro@westcoastinformatics.com>
2022-06-17 12:01:22 -0700

    RT2-979: Upgrade - Inactive Concept Count should go down as replacement concepts selected

--- src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
+++ src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
@@ -200,6 +200,12 @@ export class AdjudicateUpgradeModalComponent implements OnInit, AfterViewInit, O
       this.addRemoveConceptsComponent.refsetInternalId = this.refsetData.id;
       this.addRemoveConceptsComponent.addRemoveConceptsForAdjudication(params, params.replacementConcecpts[0]);
       this.disableAddRemove = true;
+
+      if (changeMethod == 'INACTIVE_ADDED') {
+        this.inactiveConcepts++;
+      } else if(changeMethod == 'INACTIVE_REMOVED') {
+        this.inactiveConcepts--;
+      }
     }
   }
 
@@ -403,10 +409,15 @@ export class AdjudicateUpgradeModalComponent implements OnInit, AfterViewInit, O
 
       let finalResults = [];
       let changeReportResults = [];
+      
+      this.inactiveConcepts = 0;
 
       results.items.forEach((item) => {
         for (let i = 0; i < item.replacementConcecpts.length; i++) {
           changeReportResults.push(item);
+        }
+        if (item.stillMember) {
+          this.inactiveConcepts++;
         }
       });
 
