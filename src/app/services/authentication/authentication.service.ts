15cc655b94ad861bb1025d59a6608b70f0f76dc2
Andre Jones <andrejones92@gmail.com>
2022-07-01 15:35:04 -0400

    add ability to remove cookies on logout

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -102,14 +102,27 @@ export class AuthenticationService {
         let loggedInUser = localStorage.getItem('auth_token');
         this.notAuthenticated();
 
+        localStorage.clear();
+        sessionStorage.clear();
+        this.deleteAllCookies();
         this.http.post<any>(environment.restUrl + environment.restContextPath + 'logout/' + loggedInUser, {}).subscribe(
             (data) => {
                 console.log("Back end logged out");
             }
         );
-        localStorage.clear();
-        sessionStorage.clear();
+
         window.location.href = this.generateImsUrl('logout');
+    }
+
+    private readonly deleteAllCookies = () => {
+        var cookies = document.cookie.split(";");
+    
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        }
     }
 
     isAuthenticated(): boolean {
