16de818c29564eb6b9c89ef3e08281302da0203a
Andre Jones <andrejones92@gmail.com>
2022-08-24 20:41:15 -0400

    no manual descriptions

--- src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
+++ src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
@@ -161,11 +161,7 @@ export class AdjudicateUpgradeModalComponent implements OnInit, AfterViewInit, O
       {
         field: 'replacementEnPtSection', tooltipField: 'replacementEnPtSection', headerName: 'Replacement ' + this.selectedLanguage, cellClass: 'adjudicate-column-replacementEnPtSection', minWidth: 330, width: 330, cellRenderer: 'templateRenderer', valueGetter: (params) => {
           const desc = params?.data?.replacementConcepts[0]?.descriptions;
-          return (params?.data?.replacementConcepts[0]?.reason.includes('MANUAL_REPLACEMENT') ? (this.transformManualReplacementDescriptions(desc)?.length
-            ?
-            this.transformManualReplacementDescriptions(desc)[0].term
-            : '') : (this.transformDescriptions(desc)?.length ?
-              this.transformDescriptions(desc)[0].term : ''));
+          return (this.transformDescriptions(desc)?.length ? this.transformDescriptions(desc)[0].term : '');
         }, cellRendererParams: { template: this.replacementEnPtSection }
       },
       { field: 'actionSection', tooltipField: 'actionSection', headerName: '', cellClass: 'adjudicate-column-actionSection', flex: 1, minWidth: 60, width: 60, maxWidth: 60, cellRenderer: 'templateRenderer', floatingFilter: false, cellRendererParams: { template: this.actionSection } },
