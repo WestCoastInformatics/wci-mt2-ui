3c7ef8e56df8be9a0e14ee5e9a13acdd8ecb31e8
Andre Jones <andrejones92@gmail.com>
2022-08-24 20:00:01 -0400

    adjudication complexity change 1

--- src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
+++ src/app/components/adjudicate-upgrade-modal/adjudicate-upgrade-modal.component.ts
@@ -160,11 +160,9 @@ export class AdjudicateUpgradeModalComponent implements OnInit, AfterViewInit, O
       },
       {
         field: 'replacementEnPtSection', tooltipField: 'replacementEnPtSection', headerName: 'Replacement ' + this.selectedLanguage, cellClass: 'adjudicate-column-replacementEnPtSection', minWidth: 330, width: 330, cellRenderer: 'templateRenderer', valueGetter: (params) => {
-          return (params?.data?.replacementConcepts[0]?.reason.includes('MANUAL_REPLACEMENT') ? (this.transformManualReplacementDescriptions(params?.data?.replacementConcepts[0]?.descriptions)?.length
-            ?
-            this.transformManualReplacementDescriptions(params?.data?.replacementConcepts[0]?.descriptions)[0].term
-            : '') : (this.transformDescriptions(params?.data?.replacementConcepts[0]?.descriptions)?.length ?
-              this.transformDescriptions(params?.data?.replacementConcepts[0]?.descriptions)[0].term : ''));
+          return (params?.data?.replacementConcepts[0]?.reason.includes('MANUAL_REPLACEMENT') ?
+            this.transformManualReplacementDescriptions(params?.data?.replacementConcepts[0]?.descriptions)[0]?.term
+            : this.transformDescriptions(params?.data?.replacementConcepts[0]?.descriptions)[0]?.term);
         }, cellRendererParams: { template: this.replacementEnPtSection }
       },
       { field: 'actionSection', tooltipField: 'actionSection', headerName: '', cellClass: 'adjudicate-column-actionSection', flex: 1, minWidth: 60, width: 60, maxWidth: 60, cellRenderer: 'templateRenderer', floatingFilter: false, cellRendererParams: { template: this.actionSection } },
