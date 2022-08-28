bfb71a5a121da26479911c3f2e8ec7a74d75b5ee
doublemwebsolutions <dev@doublemwebsolutions.com>
2022-08-28 05:19:03 -0600

    Comparison - RefSet Membership drop down disappears

--- src/app/components/comparison/launch-comparison-modal.component.ts
+++ src/app/components/comparison/launch-comparison-modal.component.ts
@@ -324,7 +324,7 @@ export class LaunchComparisonModalComponent {
             const selectedConcept = this.getGridRow(selectedId);
             this.loadConceptDetail(selectedConcept);
         }
-    };
+    }
 
     getGridRow(conceptId: string) {
 
@@ -413,6 +413,7 @@ export class LaunchComparisonModalComponent {
 
         this.conceptDetail = null;
         this.selectedConcept = null;
+
     }
 
     sendLoadingSpinnerTrigger = (value: any) => {
@@ -501,7 +502,7 @@ export class LaunchComparisonModalComponent {
             return;
         }
 
-        // process the comparison data with the changed members 
+        // process the comparison data with the changed members
         for (const conceptStatus of conceptStatusArray) {
 
             if (conceptStatus.failed) {
