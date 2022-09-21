f3866faefa76f96760a23cfc07e5bb360c8a4caf
Andre Jones <andrejones92@gmail.com>
2022-09-21 19:01:37 -0400

    fix resources link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -89,7 +89,7 @@ export class NavbarComponent implements OnInit {
     }
 
     resources() {
-        this.router.navigate(['dashboard']);
+        this.router.navigate(['']);
     }
 
     login() {
