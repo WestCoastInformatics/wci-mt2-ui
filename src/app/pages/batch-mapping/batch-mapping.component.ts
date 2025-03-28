088fbc2162ebca98e52f37a08fe5137966ca3c6d
Arin Sturm <asturm@westcoastinformatics.com>
2025-03-28 12:13:10 -0700

    RT2 workflow status Edit display

--- src/app/pages/batch-mapping/batch-mapping.component.ts
+++ src/app/pages/batch-mapping/batch-mapping.component.ts
@@ -119,6 +119,21 @@ export class BatchMappingComponent implements OnInit {
 
 	rowColors = [{ 'background': 'white' }, { 'background': '#f2f2f2' }];
 	currentRowColor = 0;
+	stepperInfo: any = {};
+	stepperStartInfo = {
+		'READY_FOR_EDIT_COLOR': 'details-page-stepper-unstarted-step',
+		'READY_FOR_EDIT_STARTED': false,
+		'IN_EDIT_COLOR': 'details-page-stepper-unstarted-step',
+		'IN_EDIT_STARTED': false,
+		'READY_FOR_REVIEW_COLOR': 'details-page-stepper-unstarted-step',
+		'READY_FOR_REVIEW_STARTED': false,
+		'IN_REVIEW_COLOR': 'details-page-stepper-unstarted-step',
+		'IN_REVIEW_STARTED': false,
+		'REVIEW_COMPLETED_COLOR': 'details-page-stepper-unstarted-step',
+		'REVIEW_COMPLETED_STARTED': false,
+		'READY_FOR_PUBLICATION_COLOR': 'details-page-stepper-unstarted-step',
+		'READY_FOR_PUBLICATION_STARTED': false,
+	};
 
 	refsetData: any;
 	gridOptions: any;
@@ -218,6 +233,14 @@ export class BatchMappingComponent implements OnInit {
 		}
 
 		this.disableChannel.postMessage(false);
+
+		const stepperClass = 'details-page-stepper-started-step';
+		this.stepperInfo = CodeUtility.clone(this.stepperStartInfo);
+		//his.refsetStatus?.includes('IN_EDIT')) {
+		this.stepperInfo['READY_FOR_EDIT_COLOR'] = stepperClass;
+		this.stepperInfo['READY_FOR_EDIT_STARTED'] = true;
+		this.stepperInfo['IN_EDIT_COLOR'] = stepperClass;
+		this.stepperInfo['IN_EDIT_STARTED'] = true;
 
 		this.gridOptions = {
 			context: { componentParent: this },
@@ -683,8 +706,6 @@ export class BatchMappingComponent implements OnInit {
 						});
 						results.mapEntries[b].mapAdvices = mapAdvices;
 						results.mapEntries[b].adviceAlways = adviceAlways;
-						let flag = '';
-						let lang = '';
 						data = {
 							'uuid': results.code + results.mapEntries[b].modified + b,
 							'index': results.code + count,
