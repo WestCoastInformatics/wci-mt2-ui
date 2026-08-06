1e69d5bbd9cefeb16442301f0f8be6f532246b15
Arin Sturm <asturm@westcoastinformatics.com>
2026-08-05 20:50:24 -0700

    Dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, OnInit, Output, ElementRef, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { DialogService } from 'src/app/dialog/services/dialog.service';
 import { DialogFactoryService } from 'src/app/dialog/services/dialog-factory.service';
@@ -14,7 +14,6 @@ import { UiUtility } from 'src/app/utilities/ui.utility';
 import { RefsetUtility } from 'src/app/utilities/refset.utility';
 import { Constants } from 'src/app/utilities/constants.utility';
 import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
-import { PaginationComponent } from 'src/app/components/pagination/pagination.component';
 import { Debounce } from 'src/app/decorators/debounce.decorator';
 import { User } from 'src/app/models/user';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
@@ -27,7 +26,7 @@ import { PaginationService } from 'src/app/services/pagination.service';
 	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 	user!: User;
 	searchInput = '';
 	viewOptions = [
@@ -64,16 +63,13 @@ export class DashboardComponent implements OnInit {
 	showLoadingSpinner = false;
 	toggleDropdown = false;
 	numOfResults = 0;
-	directUrl: string;
+	directUrl = '';
 	numOfMembers: any;
 	mapsetLibraryColumnStorage = 'mapsetLibraryColumnStorage';
 	disableChannel = new BroadcastChannel('disable-button-channel');
 	originalGridParams: any;
 	uiUtility = UiUtility;
 	showLoadingSearch = true;
-	toBeDevelopedModalRef!: NgbModalRef;
-	downloadModalRef!: NgbModalRef;
-	mapsetInfoModalRef!: NgbModalRef;
 	isModalOpen = false;
 	showPaging = false;
 	paginationPages: any = {};
@@ -93,18 +89,7 @@ export class DashboardComponent implements OnInit {
 
 	@Output() loadingSpinner = new EventEmitter<boolean>(true);
 
-	@ViewChild('workflowStatusSection') workflowStatus!: TemplateRef<any>;
-	@ViewChild('directoryInfoDialog') infoDialog!: TemplateRef<any>;
-	@ViewChild('directoryFeedbackDialog') feedbackDialog!: TemplateRef<any>;
-	@ViewChild('directoryInfoSection') infoSection!: TemplateRef<any>;
-	@ViewChild('directoryVersionDate') versionDate!: TemplateRef<any>;
-	@ViewChild('directoryNameSection') nameSection!: TemplateRef<any>;
-	@ViewChild('directoryActionSection') actionSection!: TemplateRef<any>;
-	@ViewChild('directoryPaging') paginationComponent!: PaginationComponent;
-	@ViewChild('directoryCategoryFilter') categoryFilter!: TemplateRef<any>;
-	@ViewChild('directoryWorkflowStatusSection') versionStatus!: TemplateRef<any>;
-	@ViewChild('directorySearchInput') private directorySearchInput!: ElementRef;
-	@ViewChild('downloadModal') downloadModal!: TemplateRef<any>;
+	@ViewChild('workflowStatusSection') workflowStatusSection!: TemplateRef<any>;
 
 	constructor(
 		private router: Router,
@@ -129,9 +114,14 @@ export class DashboardComponent implements OnInit {
 		this.titleService.setTitle('Mapping Tool - Dashboard');
 		this.breadcrumbService.setBreadcrumbs([{ label: 'Dashboard' }]);
 		this.clearSavedSelections();
-		this.getMapsetData();
 		this.getModuleMetadata();
 		this.disableChannel.postMessage(false);
+	}
+
+	ngAfterViewInit() {
+		if (this.workflowStatusSection) {
+			this.getMapsetData();
+		}
 	}
 
 	private clearSavedSelections(): void {
@@ -190,8 +180,8 @@ export class DashboardComponent implements OnInit {
 				suppressSorting: true,
 			},
 			{
-				field: 'code',
-				tooltipField: 'code',
+				field: 'conceptCode',
+				tooltipField: 'conceptCode',
 				headerName: 'Source',
 				headerTooltip: 'Source',
 				flex: 1,
@@ -202,32 +192,29 @@ export class DashboardComponent implements OnInit {
 				suppressSorting: true,
 			},
 			{
-				field: 'name',
-				tooltipField: 'name',
+				field: 'conceptName',
+				tooltipField: 'conceptName',
 				headerName: 'Source PT',
-				headerTooltip: 'Source PT',
+				cellClass: 'rt2-directory-column-name',
 				flex: 2,
 				resizable: true,
-				minWidth: 165,
-				cellRenderer: TemplateRendererComponent,
-				cellRendererParams: { template: this.nameSection },
+				minWidth: 65,
+				sort: 'asc',
 				sortable: false,
 				unSortIcon: false,
 				suppressSorting: true,
 			},
 			{
-				field: 'versionStatus',
-				tooltipField: 'versionStatus',
+				field: 'workflowStatus',
+				tooltipField: 'workflowStatus',
 				headerName: 'Workflow Status',
 				cellClass: 'rt2-directory-column-version-status',
 				minWidth: 165,
 				width: 200,
 				resizable: true,
 				cellRenderer: TemplateRendererComponent,
-				cellRendererParams: { template: this.workflowStatus },
-				sortable: false,
-				unSortIcon: false,
-				suppressSorting: true,
+				cellRendererParams: { template: this.workflowStatusSection },
+				unSortIcon: true,
 			},
 			{
 				field: 'modified',
@@ -293,10 +280,6 @@ export class DashboardComponent implements OnInit {
 		this.toggleDropdown = !this.toggleDropdown;
 	}
 
-	menuOpened() {
-		this.directorySearchInput.nativeElement.focus();
-	}
-
 	//***** AG Grid Functions *****/
 	onGridReady = (gridReadyParams: any) => {
 		this.originalGridParams = gridReadyParams;
@@ -334,11 +317,23 @@ export class DashboardComponent implements OnInit {
 			next: (results) => {
 				this.showLoadingSearch = false;
 				const data = results;
-
-				this.refsetData = data;
+				let conceptCodes = [];
+				this.refsetData = [];
+				for (const item of data.items) {
+					let mapping = {
+						modified: item.modified,
+						refSetCode: item.mapSet.refSetCode,
+						refSetName: item.mapSet.refSetName,
+						conceptCode: item.sourceConceptCode,
+						conceptName: item.sourceConceptName,
+						workflowStatus: item.workflowStatus.replace('_', ' '),
+					};
+					conceptCodes.push(item.sourceConceptCode);
+					this.refsetData.push(mapping);
+				}
 				this.numOfMembers = results.total;
 				this.numOfResults = results.total;
-
+				results.items = this.refsetData;
 				const lastIndex = document.getElementsByClassName('ag-header').length - 1;
 				const child = document.getElementsByClassName('ag-header')[lastIndex];
 				document.getElementById('directoryHeader').appendChild(child);
@@ -350,17 +345,13 @@ export class DashboardComponent implements OnInit {
 					if (pageNumber > 1) {
 						this.refsetGridPaging.totalRows = this.refsetGridApi.paginationGetPageSize() * (pageNumber - 1);
 						this.refsetGridPaging.totalKnown = true;
-						this.paginationComponent.goToPage(pageNumber - 1);
 					}
 					this.showPaging = false;
 					return;
 				} else {
 					this.showPaging = true;
 				}
-				if (localStorage.getItem('librarySearchInput')) {
-					this.searchInput = JSON.parse(localStorage.getItem('librarySearchInput'));
-					this.refsetGridApi.setGridOption('quickFilterText', this.searchInput);
-				}
+
 				UiUtility.applyServerPagedGridResults(results, this.refsetGridApi, this.refsetGridPaging, pageNumber, null, false);
 			},
 			error: (error: any) => {
@@ -413,20 +404,23 @@ export class DashboardComponent implements OnInit {
 			//
 		} else {
 			const selectedRows = this.refsetGridApi.getSelectedRows();
-			let selectedId: string;
-			let selectedVersionDate: string;
+			let mappingCode: string;
 			let selectedCode: string;
 
 			selectedRows.forEach(function (selectedRow: any, index: any) {
-				selectedId = selectedRow.refsetId;
 				selectedCode = selectedRow.refSetCode;
-				selectedVersionDate = RefsetUtility.getVersionDateForRefsetApiCall(selectedRow);
+				mappingCode = selectedRow.conceptCode;
 			});
-
-			//this.goToDetailsPage(selectedId, selectedVersionDate);
-			this.goToMapRecordsPage(selectedCode);
+			this.goToMappingPage(selectedCode, mappingCode);
 		}
 	};
+
+	goToMappingPage(code: any, mappingCode: any) {
+		this.router.navigate(['/projects' + '/mapset/' + code + '/mapping/' + mappingCode], {
+			replaceUrl: false,
+			skipLocationChange: false,
+		});
+	}
 
 	@Debounce()
 	changedViewFilter() {
@@ -453,129 +447,6 @@ export class DashboardComponent implements OnInit {
 	}
 
 	//***** General Functions *****/
-
-	openEclBuilder(fieldId: any) {
-		UiUtility.openEclBuilder(fieldId, 'MAIN');
-	}
-
-	downloadMapsets(params: any) {
-		this.mapsetInfo = params.data;
-		this.downloadTitle = 'Download ' + this.mapsetInfo.refSetCode + ' ' + this.mapsetInfo.refSetName;
-		this.types = [
-			{ value: 'SNAPSHOT', display: 'SNAPSHOT' },
-			{ value: 'DELTA', display: 'DELTA' },
-		];
-		this.formats = [
-			{ value: 'RF2', display: 'RF2' },
-			{ value: 'RF2_WITH_NAMES', display: 'RF2 With Names' },
-			{ value: 'SCTIDS', display: 'List Of SCTIDs' },
-		];
-		this.openDownloadModal(this.downloadModal);
-	}
-
-	startDownload() {
-		this.downloadError = '';
-		if (this.selectedFormat['value'] !== undefined && this.selectedType['value'] !== undefined) {
-			this.downloading = true;
-			this.refsetService.getMapsetsByCode(this.mapsetInfo.refSetCode).subscribe((results) => {
-				this.mapsetInfo = results;
-				const params = {
-					branch: this.mapsetInfo.branchPath,
-					mapSetCode: this.mapsetInfo.refSetCode,
-					fileFormatType: this.selectedType['value'],
-					fileExportType: this.selectedFormat['value'],
-					fileNameDate: CodeUtility.getCurrentDate().split('-').join(''),
-					languageId: this.mapsetInfo.moduleId,
-					startEffectiveTime: this.mapsetInfo.version.replaceAll('-', ''),
-					transientEffectiveTime: this.mapsetInfo.version.replaceAll('-', ''),
-					exportMetadata: this.selectExportMetadata,
-				};
-				this.refsetService.exportMapset(params).subscribe(
-					(data: any) => {
-						this.getMapsetDownloadStatus(data.url);
-					},
-					(err: any) => {
-						this.downloading = false;
-						console.error(' Error: ', err);
-						this.notificationService.show('Error downloading, please try again.', 'Error', 'error', {
-							timeOut: 1500,
-							extendedTimeOut: 0,
-						});
-					},
-				);
-			});
-		} else {
-			this.downloadError = 'Please select a download type and format.';
-		}
-	}
-
-	getMapsetDownloadStatus(url: string) {
-		this.refsetService.getDownloadMapsetStatus(url).subscribe(
-			(data) => {
-				const a = document.createElement('a');
-				switch (data.status) {
-					case 'FAILED':
-						this.downloading = false;
-						this.notificationService.show('Failed to download Mapset.', 'Error', 'error', { timeOut: 3000, extendedTimeOut: 0 });
-						this.closeDownloadModal();
-						break;
-					case 'COMPLETED':
-						a.href = this.refsetService.contextPath + data.result;
-						a.download = url.split('/').pop();
-						document.body.appendChild(a);
-						a.click();
-						document.body.removeChild(a);
-						this.downloading = false;
-						this.closeDownloadModal();
-						break;
-					default:
-						setTimeout(() => {
-							this.getMapsetDownloadStatus(url);
-						}, 200);
-				}
-			},
-			(err: any) => {
-				this.downloading = false;
-				console.error(' Error: ', err);
-				this.notificationService.show('Error downloading, please try again.', 'Error', 'error', { timeOut: 1500, extendedTimeOut: 0 });
-			},
-		);
-	}
-
-	openDownloadModal(content: any) {
-		this.downloadModalRef = this.modalService.open(content, { centered: true });
-		this.isModalOpen = true;
-	}
-
-	closeDownloadModal() {
-		this.downloadError = '';
-		this.selectedFormat = {};
-		this.selectedType = {};
-		this.selectExportMetadata = false;
-		this.downloadModalRef.close();
-		this.isModalOpen = false;
-	}
-
-	openMapsetInfoModal(params: any, content: any) {
-		this.mapsetInfo = params.data;
-		this.mapsetInfoModalRef = this.modalService.open(content, { size: 'lg', centered: true });
-		this.isModalOpen = true;
-	}
-
-	closeMapsetInfoModal() {
-		this.mapsetInfoModalRef.close();
-		this.isModalOpen = false;
-	}
-
-	openToBeDevelopedModal(content: any) {
-		this.toBeDevelopedModalRef = this.modalService.open(content, { centered: true });
-		this.isModalOpen = true;
-	}
-
-	closeToBeDevelopedModal() {
-		this.toBeDevelopedModalRef.close();
-		this.isModalOpen = false;
-	}
 
 	goToDetailsPage(refsetId: any, versionDate: any) {
 		const url = new URL(window.location.href);
@@ -640,30 +511,6 @@ export class DashboardComponent implements OnInit {
 		} else {
 			this.moduleMetadata = this.mt2Service.moduleMetadata.value;
 		}
-	}
-
-	openFeedback(refsetId: string) {
-		const refset = this.getRefsetRow(refsetId);
-		const dialogId = 'directoryFeedbackDialog';
-
-		const dialogData = {
-			headerText: `Reference Set Feedback for ${refset.name} (${refset.refsetId})`,
-			template: this.feedbackDialog,
-			data: refset,
-		};
-
-		const dialogOptions = {
-			id: dialogId,
-			disableClose: false,
-		};
-
-		this.dialog = this.dialogFactoryService.open(dialogData, dialogOptions);
-
-		this.dialog.confirmed().subscribe((data) => {
-			if (data) {
-				refset.feedback = data.feedback;
-			}
-		});
 	}
 
 	setFullNarrativeText(show: boolean): void {
